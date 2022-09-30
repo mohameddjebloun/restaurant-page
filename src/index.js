6dbc281e6530d5a6293ba213e1d989dbc4fa2bd6
Mohamed Djebloun <djeblounm@gmail.com>
2022-09-30 16:59:09 +0100

    Import and call load_home

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 //Import the necessary modules
 import "./style.css";
 import initial_load from "./initial_load";
+import load_home from "./load_home";
 //Load the initial page
 initial_load();
+load_home();
