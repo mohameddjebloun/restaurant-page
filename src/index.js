d05be88f5e06f7b9abaf28f10fb1aaf0b849f9b0
Mohamed Djebloun <djeblounm@gmail.com>
2022-10-13 23:38:38 +0100

    Import and call load_contact

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import "./style.css";
 import initial_load from "./initial_load";
 import load_home from "./load_home";
 import load_menu from "./load_menu";
+import load_contact from "./load_contact";
 //Load the initial page
 initial_load();
-load_menu();
+load_contact();
+//load_menu();
 //load_home();
