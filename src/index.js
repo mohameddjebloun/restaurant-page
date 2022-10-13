5937809c3bed01b1027fe6166f2250474e5c484d
Mohamed Djebloun <djeblounm@gmail.com>
2022-10-13 22:48:20 +0100

    Import and call load_menu

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import "./style.css";
 import initial_load from "./initial_load";
 import load_home from "./load_home";
+import load_menu from "./load_menu";
 //Load the initial page
 initial_load();
-load_home();
+load_menu();
+//load_home();
